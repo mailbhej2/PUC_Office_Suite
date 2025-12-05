6ed5cacafcb4ccd0447f92d0f5da937976f919f8
deepak <mailbhej2@gmail.com>
2025-12-05 13:47:00 +0530

    nav button for file status

--- pages/1_File_Status.py
+++ pages/1_File_Status.py
@@ -3,13 +3,15 @@ import pandas as pd
 import os
 from dotenv import load_dotenv
 
+st.set_page_config(layout="wide")
+
 st.set_page_config(page_title="File Status")
 st.title("File Status")
 load_dotenv()
 
 # Read Google Sheet
 df = pd.read_csv(os.getenv("GOOGLE_SHEET_URL"))
-
+df.index += 2
 # 🔹 Specify which columns you want to show
 # Just change this list anytime
 columns_to_show = [
@@ -26,4 +28,5 @@ columns_to_show = [
 # Filter only available columns (avoids errors)
 available_cols = [col for col in columns_to_show if col in df.columns]
 
+st.title("File Status")
 st.dataframe(df[available_cols])
