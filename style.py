aa484e07080e6f35c08bd8b3c2ed75743476ddcf
deepak <mailbhej2@gmail.com>
2026-05-25 10:42:36 +0530

    added task planner

--- style.py
+++ style.py
@@ -10,19 +10,7 @@ def local_css():
     .stAppHeader{
         background-color: #4fae79 !important;
     }
-    button.st-emotion-cache-1anq8dj {
-        background-color: #4fae79 !important;
-        color: white !important;
-        border-radius: 6px !important;
-        padding: 10px 20px !important;
-        font-size: 16px !important;
-        border: none !important;
-        cursor: pointer !important;
-        transition: background-color 0.3s ease;
-    }
-    button.st-emotion-cache-1anq8dj:hover {
-        background-color: #999999 !important;
-    }
+  
     .stAlertContainer{
         background-color: #2a8359 !important;
     }
